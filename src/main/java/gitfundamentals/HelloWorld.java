47020f1faa5526876b0c48307342e89990dbeef3
Rajendra Upadhyay <rajendra.upadhayay@gmail.com>
2023-07-02 21:59:18 +0530

    Revert "added new line in test1"
    This reverts commit f3c5d24ea04ecbc7078c3eedb99ec489c881d3a3.

--- src/main/java/gitfundamentals/HelloWorld.java
+++ src/main/java/gitfundamentals/HelloWorld.java
@@ -6,10 +6,9 @@ public class HelloWorld {
         System.out.println("added this line in master");
         System.out.println("added this line in test1 branch");
         System.out.println("added this line in master to check conflict");
-        System.out.println("adding this message and to test undo operation");
+        System.out.println("adding line1 to undo changes");
+        System.out.println("adding line2 to undo changes");
         System.out.println("adding line3 to not undo");
-        System.out.println("adding line4 to not undo");
-        System.out.println("adding line5 to not undo");
     }
     public void test1Method(){
         System.out.println("this method is added in test1 branch");
