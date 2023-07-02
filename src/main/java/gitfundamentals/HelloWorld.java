378912d7db494e806c2f44b241e36d217cf8df07
Rajendra Upadhyay <rajendra.upadhayay@gmail.com>
2023-07-02 18:48:32 +0530

    initial commit

---
+++ src/main/java/gitfundamentals/HelloWorld.java
@@ -0,0 +1,7 @@
+package gitfundamentals;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Jai Shree Ganesh...Om Gam Ganpatye Namah...");
+    }
+}
