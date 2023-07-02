bf660c601cbfed1146b8640c54b06f3b7e86895f
Rajendra Upadhyay <rajendra.upadhayay@gmail.com>
2023-07-02 19:05:50 +0530

    added new method

--- src/main/java/gitfundamentals/HelloWorld.java
+++ src/main/java/gitfundamentals/HelloWorld.java
@@ -3,8 +3,12 @@ package gitfundamentals;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Jai Shree Ganesh...Om Gam Ganpatye Namah...");
+        System.out.println("added this line in master");
     }
     public void test1Method(){
         System.out.println("this method is added in test1 branch");
+    }
+    public void addedthisInMaster(){
+        System.out.println("added this method in master");
     }
 }
