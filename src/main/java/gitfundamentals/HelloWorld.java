c2723c2ab8ecb225a4c7f4a38c27649f0c23c2e7
Rajendra Upadhyay <rajendra.upadhayay@gmail.com>
2023-07-02 19:02:48 +0530

    added new method

--- src/main/java/gitfundamentals/HelloWorld.java
+++ src/main/java/gitfundamentals/HelloWorld.java
@@ -4,4 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Jai Shree Ganesh...Om Gam Ganpatye Namah...");
     }
+    public void test1Method(){
+        System.out.println("this method is added in test1 branch");
+    }
 }
